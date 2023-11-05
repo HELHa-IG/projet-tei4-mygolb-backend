fc909954dc1f19836da05fc7c7942d09da860fc2
Assabban Yassin <la200036@student.helha.be>
2023-11-05 21:58:53 +0100

    Model User Creer

---
+++ MyGolb/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+namespace MyGolb.Models;
+
+public class User
+{
+    public long Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    
+    [JsonIgnore]
+    public virtual List<Post>? Posts { get; set; }
+    [JsonIgnore]
+    public virtual List<Comment>? Comments { get; set; }
+    [JsonIgnore]
+    public virtual List<Interaction>? Interactions { get; set; }
+}
