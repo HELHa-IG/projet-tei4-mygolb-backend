154aaa4e09a29a2d856d58acd27241c22c74f73a
Assabban Yassin <la200036@student.helha.be>
2023-11-05 22:14:37 +0100

    feat: Cors policies

--- MyGolb/Program.cs
+++ MyGolb/Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "MyGolbPolicy",
+        policy =>
+        {
+            policy.WithOrigins("*")
+                .WithMethods("*")
+                .WithHeaders("*");
+        });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,6 +27,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
